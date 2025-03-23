501d78e5f23d635c10b1fc0a377b8be37764b9db
razenry <razenry.code@gmail.com>
2025-03-23 11:14:34 +0700

    Update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@news-portal-main-hqpyhy.laravel.cloud') && $this->hasVerifiedEmail();
+        return true; // str_ends_with($this->email, '@news-portal-main-hqpyhy.laravel.cloud') && $this->hasVerifiedEmail();
     }
 
     /**
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            // ->emailVerification()
             ->colors([
                 'primary' => Color::Amber,
             ])
