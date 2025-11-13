06aa98b121f12faf8f9579947d9014a970e56212
Razenry <razenry.code@gmail.com>
2025-11-13 11:35:51 +0700

    Update footer.blade.php

--- resources/views/livewire/layout/footer.blade.php
+++ resources/views/livewire/layout/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="md:flex md:justify-between">
             <div class="mb-6 md:mb-0">
                 <a href="" class="flex items-center">
-                    <img src="{{ asset($settings?->icon ? 'storage/' . $settings?->icon : 'logo.png') }}" class="mr-3 h-8" alt="FlowBite Logo" />
+                    <img src="{{ asset($settings?->icon ? 'storage/' . $settings?->icon : 'logo.png') }}" class="mr-3 h-8" alt="Amanah Voice" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{{ $settings->name ?? "News Portal" }}</span>
                 </a>
             </div>
